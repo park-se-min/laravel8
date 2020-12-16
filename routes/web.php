7c906c351ca19fa267b99db484cc66ea1eb785fc
park-se-min <pseeq@naver.com>
2020-12-16 10:33:44 +0900

    2020.12.16 - {51p:라우팅}

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/{id}/{asdf?}', function ($id='', $asdf='') {
+	echo $id;
+	echo "<br>";
+	echo $asdf;
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::resource('user', 'UserController')->name('artice');
+// Route::get('user/profile', [UserProfileController::class, 'show'])->name('profile');
