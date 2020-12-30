7a44119e056d04ef7ae5ed480cdd2f588f203439
park-se-min <pseeq@naver.com>
2020-12-30 18:07:07 +0900

    2020.12.30 - {p79:'인증'}

---
+++ app/Http/Livewire/SalesOrdersStatusBoard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SalesOrdersStatusBoard extends Component
+{
+    public function render()
+    {
+        return view('livewire.sales-orders-status-board');
+    }
+}
